ff65dc9639c3f2390e1cc86228c6ba4c57d157a8
rookiedragon <770632586@qq.com>
2020-05-25 00:02:40 +0800

    Add files via upload

---
+++ Util.java
@@ -0,0 +1,15 @@
+package aa;
+import java.security.SecureRandom;
+public class Util {
+	public static String getRandomString(int length) {
+		//String str = "zxcvbnmlkjhgfdsaqwertyuiopQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+		String str = "0123456789abcdef";
+		SecureRandom random1 = new SecureRandom();
+		String res = "";
+		for(int i =0;i<length;i++) {
+			int number = random1.nextInt(16);
+			res += str.charAt(number);
+		}
+		return res;
+	}
+}
